0d84c2a09f92d6f4ac1546aca8783573b690f632
Shivkumar Konade <shivkumarkonade@Shivkumars-MacBook-Air.local>
2021-12-20 21:19:53 +0530

    updated

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,17 +1,138 @@
 import 'package:delivery_m/ui/home/widgets/drawer.dart';
+import 'package:delivery_m/ui/home/widgets/my_calendar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme;
     return Scaffold(
       drawer: MyDrawer(),
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: ListView(),
+      body: Column(
+        children: [
+          MyCalendar(),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(4),
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: Row(
+                            children: [
+                              AnaCard(),
+                              AnaCard(),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: Row(
+                            children: [
+                              AnaCard(),
+                              AnaCard(),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Shivkumar Konade',
+                            style: style.subtitle2,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: Row(
+                            children: [
+                              AnaCard(),
+                              AnaCard(),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: Row(
+                            children: [
+                              AnaCard(),
+                              AnaCard(),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AnaCard extends StatelessWidget {
+  const AnaCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme;
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(4),
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 16,
+            ),
+            SizedBox(width: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Water Bottle',
+                  style: style.caption,
+                ),
+                SizedBox(width: 8),
+                Row(
+                  children: [
+                    Text('10'),
+                    Text('/'),
+                    Text('20'),
+                    Text('/'),
+                    Text('30'),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/ui/home/widgets/my_calendar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import '../../../../utils/dates.dart';
+import '../../../../utils/utils.dart';
+
+class MyCalendar extends StatelessWidget {
+  const MyCalendar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final calenderStyle = CalendarStyle();
+    // final model = context.read(scheduleViewModelProvider);/
+    return Card(
+      margin: EdgeInsets.all(0),
+      child: TableCalendar(
+        calendarStyle: CalendarStyle(
+          selectedDecoration: BoxDecoration(
+            color: theme.accentColor,
+            shape: BoxShape.circle,
+          ),
+          todayTextStyle: calenderStyle.defaultTextStyle,
+          todayDecoration: BoxDecoration(
+            color: Colors.transparent,
+            border: Border.all(color: theme.primaryColor, width: 1),
+            shape: BoxShape.circle,
+          ),
+        ),
+        selectedDayPredicate: (d) => isSameDay(DateTime.now(), d),
+        onDaySelected: (d1, d2) {
+          // print(d1);
+          // model.selectedDate = DateTime(d1.year, d1.month, d1.day);
+        },
+        // onPageChanged: (d2) => model.focusDate = d2,
+        daysOfWeekStyle: DaysOfWeekStyle(
+          dowTextFormatter: (d, e) => Utils.weekD(d),
+        ),
+        headerStyle: HeaderStyle(
+          formatButtonVisible: false,
+          titleCentered: true,
+        ),
+        availableCalendarFormats: {
+          CalendarFormat.week: "Week",
+        },
+        calendarFormat: CalendarFormat.week,
+        focusedDay: DateTime.now(),
+        firstDay: Dates.today.subtract(
+          Duration(days: 30),
+        ),
+        lastDay: DateTime(2025),
+      ),
+    );
+  }
+}
