71da98ca6a6430043382a90896a3673e5880c0a2
Shivkumar Konade <shivkumarkonade@Shivkumars-MacBook-Air.local>
2022-03-05 18:34:09 +0530

    updated

--- lib/core/providers/root_view_model_provider.dart
+++ lib/core/providers/root_view_model_provider.dart
@@ -6,7 +6,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 final rootViewModelProvider =
     Provider.family<void, BuildContext>((ref, context) {
   ref.watch(masterDataProvider).whenData((value) {
-    if (value.versionCode > 1) {
+    if (value.versionCode > 2) {
       showDialog(
           context: context,
           builder: (context) => VersionDialog(forced: value.forced),
